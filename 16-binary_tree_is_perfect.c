bbeeffc0b21c3ceae107b38b8f376ca5eb065d4e
Friasru <albertofriasru@gmail.com>
2026-04-12 16:20:32 -0700

    Fix indentation and rename helper function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -43,12 +43,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (left_height != right_height)
 	return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+	if (tree->left == NULL && tree->right == NULL)
+	return (1);
 
-    if (tree->left == NULL || tree->right == NULL)
-        return (0);
+	if (tree->left == NULL || tree->right == NULL)
+	return (0);
 
-    return (binary_tree_is_perfect(tree->left) &&
-        binary_tree_is_perfect(tree->right));
+	return (binary_tree_is_perfect(tree->left) &&
+	binary_tree_is_perfect(tree->right));
 }
