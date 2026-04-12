94d2b3eea5c7f80ccc9e490f3a94899d622ee8b0
Friasru <albertofriasru@gmail.com>
2026-04-12 16:17:14 -0700

    Fix indentation and rename helper function in 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,19 +8,19 @@
  */
 int get_height(const binary_tree_t *tree)
 {
-    int left_height;
-    int right_height;
+	int left_height;
+	int right_height;
 
-    if (tree == NULL)
-        return (-1);
+	if (tree == NULL)
+	return (-1);
 
-    left_height = get_height(tree->left);
-    right_height = get_height(tree->right);
+	left_height = get_height(tree->left);
+	right_height = get_height(tree->right);
 
-    if (left_height > right_height)
-        return (left_height + 1);
-    else
-        return (right_height + 1);
+	if (left_height > right_height)
+	return (left_height + 1);
+	else
+	return (right_height + 1);
 }
 
 /**
@@ -31,14 +31,14 @@ int get_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_height;
-    int right_height;
+	int left_height;
+	int right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+	return (0);
 
-    left_height = get_height(tree->left);
-    right_height = get_height(tree->right);
+	left_height = get_height(tree->left);
+	right_height = get_height(tree->right);
 
-    return (left_height - right_height);
+	return (left_height - right_height);
 }
