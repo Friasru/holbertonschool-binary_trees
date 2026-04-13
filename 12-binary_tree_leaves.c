cf8b77290d025009ed6b16264027c9d7afc5bd10
Friasru <albertofriasru@gmail.com>
2026-04-12 19:09:39 -0700

    removed space at the start of the line 9

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,7 +6,7 @@
  *
  * Return: The number of leaves in the tree, or 0 if tree is NULL
  */
-  size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 	return (0);
